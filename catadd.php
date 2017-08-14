b7bf53cf00c43e0bd53ac63241b02ee820c51c29
Hider <724598894@qq.com>
2017-08-14 17:28:28 +0800

    0.01

---
+++ catadd.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/14
+ * Time: 16:40
+ */
---
+++ index.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/14
+ * Time: 16:02
+ */
+
+include("./view/front/index.html");
+?>
