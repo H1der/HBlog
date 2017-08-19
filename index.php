231ee404fb669fa515fde074b038eafe3aa0c830
Hider <724598894@qq.com>
2017-08-19 21:47:32 +0800

    0.01

--- index.php
+++ index.php
@@ -5,13 +5,22 @@
  * Date: 2017/8/14
  * Time: 16:02
  */
-
+//获取文章列表
 include ('./lib/init.php');
-$sql = 'select art_id,cat_id,user_id,nick,pubtime,title,content from art order by art_id desc';
-$art = mGetAll($sql);
 
+//判断地址栏是否有cat_id
+if(isset($_GET['cat_id'])){
+    $where = " and art.cat_id=$_GET[cat_id]";
+}else{
+    $where ='';
+}
+//获取栏目信息
 $sql = 'select cat_id,catname from cat';
 $cats = mGetAll($sql);
 
+$sql = "select art_id,title,content,pubtime,comm,catname from art inner join cat on art.cat_id=cat.cat_id where 1" . $where;
+$art = mGetAll($sql);
+
+//print_r($art);exit();
 
 include (ROOT.'./view/front/index.html');
